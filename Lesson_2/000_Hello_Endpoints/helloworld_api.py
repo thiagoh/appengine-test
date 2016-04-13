7ad4bd08c8edec8c1a8974b007c788c14ef87d2a
Thiago Andrade <thiagoh@gmail.com>
2016-04-13 19:16:01 -0300

    new endpoints and fields in lesson 2: age

--- Lesson_2/000_Hello_Endpoints/helloworld_api.py
+++ Lesson_2/000_Hello_Endpoints/helloworld_api.py
@@ -9,36 +9,37 @@ from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
 
-
 # If the request contains path or querystring arguments,
 # you cannot use a simple Message class.
 # Instead, you must use a ResourceContainer class
 REQUEST_CONTAINER = endpoints.ResourceContainer(
     message_types.VoidMessage,
     name=messages.StringField(1),
+    age=messages.IntegerField(2),
 )
 
-
 package = 'Hello'
-
 
 class Hello(messages.Message):
     """String that stores a message."""
     greeting = messages.StringField(1)
 
-
 @endpoints.api(name='helloworldendpoints', version='v1')
 class HelloWorldApi(remote.Service):
     """Helloworld API v1."""
 
-    @endpoints.method(message_types.VoidMessage, Hello, path = "sayHello", http_method='GET', name = "sayHello")
+    @endpoints.method(message_types.VoidMessage, Hello, path="sayHello", http_method='GET', name="sayHello")
     def say_hello(self, request):
       return Hello(greeting="Hello World")
 
-    @endpoints.method(REQUEST_CONTAINER, Hello, path = "sayHelloByName", http_method='GET', name = "sayHelloByName")
+    @endpoints.method(REQUEST_CONTAINER, Hello, path="sayHelloByName", http_method='GET', name="sayHelloByName")
     def say_hello_by_name(self, request):
-      greet = "Hello {}".format(request.name)
+      greet = "This is a great hello from the server to {}".format(request.name)
       return Hello(greeting=greet)
 
+    @endpoints.method(REQUEST_CONTAINER, Hello, path="sayHelloWithAge", http_method='GET', name="sayHelloWithAge")
+    def say_hello_with_age(self, request):
+      greet = "Your age is {}".format(request.age)
+      return Hello(greeting=greet)
 
 APPLICATION = endpoints.api_server([HelloWorldApi])
--- Lesson_2/000_Hello_Endpoints/static/hello.js
+++ Lesson_2/000_Hello_Endpoints/static/hello.js
@@ -1,92 +1,123 @@
-/*
- * http://stackoverflow.com/questions/18260815/use-gapi-client-javascript-to-execute-my-custom-google-api
- * https://developers.google.com/appengine/docs/java/endpoints/consume_js
- * https://developers.google.com/api-client-library/javascript/reference/referencedocs#gapiclientload
- *
- */
+(function(argument) {
 
-/**
- * After the client library has loaded, this init() function is called.
- * The init() function loads the helloworldendpoints API.
- */
+	'use strict';
 
-function init() {
-	
-	// You need to pass the root path when you load your API
-	// otherwise calls to execute the API run into a problem
-	
-	// rootpath will evaulate to either of these, depending on where the app is running:
-	// //localhost:8080/_ah/api
-	// //your-app-id/_ah/api
+	/*
+	 * http://stackoverflow.com/questions/18260815/use-gapi-client-javascript-to-execute-my-custom-google-api
+	 * https://developers.google.com/appengine/docs/java/endpoints/consume_js
+	 * https://developers.google.com/api-client-library/javascript/reference/referencedocs#gapiclientload
+	 *
+	 */
 
-	var rootpath = "//" + window.location.host + "/_ah/api";
-	
-	// Load the helloworldendpoints API
-	// If loading completes successfully, call loadCallback function
-	gapi.client.load('helloworldendpoints', 'v1', loadCallback, rootpath);
-}
+	/**
+	 * After the client library has loaded, this init() function is called.
+	 * The init() function loads the helloworldendpoints API.
+	 */
 
-/*
- * When helloworldendpoints API has loaded, this callback is called.
- * 
- * We need to wait until the helloworldendpoints API has loaded to
- * enable the actions for the buttons in index.html,
- * because the buttons call functions in the helloworldendpoints API
- */
-function loadCallback () {	
-	// Enable the button actions
-	enableButtons ();
-}
+	function init() {
 
-function enableButtons () {
-	// Set the onclick action for the first button
-	btn = document.getElementById("input_greet_generically");
-	btn.onclick= function(){greetGenerically();};
-	
-	// Update the button label now that the button is active
-	btn.value="Click me for a generic greeting";
-	
-	// Set the onclick action for the second button
-	btn = document.getElementById("input_greet_by_name");
-	btn.onclick=function(){greetByName();};
-	
-	// Update the button label now that the button is active
-	btn.value="Click me for a personal greeting";
-}
+		// You need to pass the root path when you load your API
+		// otherwise calls to execute the API run into a problem
 
-/*
- * Execute a request to the sayHello() endpoints function
- */
-function greetGenerically () {
-	// Construct the request for the sayHello() function
-	var request = gapi.client.helloworldendpoints.sayHello();
-	
-	// Execute the request.
-	// On success, pass the response to sayHelloCallback()
-	request.execute(sayHelloCallback);
-}
+		// rootpath will evaulate to either of these, depending on where the app is running:
+		// //localhost:8080/_ah/api
+		// //your-app-id/_ah/api
 
-/*
- * Execute a request to the sayHelloByName() endpoints function.
- * Illustrates calling an endpoints function that takes an argument.
- */
-function greetByName () {
-	// Get the name from the name_field element
-	var name = document.getElementById("name_field").value;
-	
-	// Call the sayHelloByName() function.
-	// It takes one argument "name"
-	// On success, pass the response to sayHelloCallback()
-	var request = gapi.client.helloworldendpoints.sayHelloByName({'name': name});
-	request.execute(sayHelloCallback);
-}
+		var rootpath = "//" + window.location.host + "/_ah/api";
 
-// Process the JSON response
-// In this case, just show an alert dialog box
-// displaying the value of the message field in the response
-function sayHelloCallback (response) {
-	alert(response.greeting);	
-}
+		// Load the helloworldendpoints API
+		// If loading completes successfully, call loadCallback function
+		gapi.client.load('helloworldendpoints', 'v1', function() {
 
+			/*
+			 * When helloworldendpoints API has loaded, this callback is called.
+			 * 
+			 * We need to wait until the helloworldendpoints API has loaded to
+			 * enable the actions for the buttons in index.html,
+			 * because the buttons call functions in the helloworldendpoints API
+			 */
+			// Enable the button actions
+			enableButtons();
 
+			console.log('loaded!');
 
+		}, rootpath);
+
+		console.log('initted!');
+	}
+
+	window.init = init;
+
+	function enableButtons() {
+
+		var btn1 = document.getElementById("input_greet_generically");
+		btn1.value = "Click me for a generic greeting";
+		
+		var btn2 = document.getElementById("input_greet_by_name");
+		btn2.value = "Click me for a personal greeting";
+
+		var btn3 = document.getElementById("input_age");
+		btn3.value = "Click me for a personal greeting about your age";
+
+		btn1.onclick = function() {
+
+			/*
+			 * Execute a request to the sayHello() endpoints function
+			 */
+			// Construct the request for the sayHello() function
+			var request = gapi.client.helloworldendpoints.sayHello();
+
+			// Execute the request. On success, pass the response to sayHelloCallback()
+			request.execute(sayHelloCallback);
+		};
+
+		// Set the onclick action for the second button
+		btn2.onclick = function() {
+
+			/*
+			 * Execute a request to the sayHelloByName() endpoints function.
+			 * Illustrates calling an endpoints function that takes an argument.
+			 */
+			// Get the name from the name_field element
+			var name = document.getElementById("name_field").value;
+
+			// Call the sayHelloByName() function.
+			// It takes one argument "name"
+			// On success, pass the response to sayHelloCallback()
+			var request = gapi.client.helloworldendpoints.sayHelloByName({
+				'name': name
+			});
+
+			request.execute(sayHelloCallback);
+		};
+
+		// Set the onclick action for the second button
+		btn3.onclick = function() {
+
+			/*
+			 * Execute a request to the sayHelloWithAge() endpoints function.
+			 * Illustrates calling an endpoints function that takes an argument.
+			 */
+			// Get the name from the name_field element
+			var age = document.getElementById("age_field").value;
+
+			// Call the sayHelloWithAge() function.
+			// It takes one argument "name"
+			// On success, pass the response to sayHelloCallback()
+			var request = gapi.client.helloworldendpoints.sayHelloWithAge({
+				'age': age
+			});
+
+			request.execute(function(response) {
+				alert(response.greeting);
+			});
+		};
+	}
+
+	// Process the JSON response
+	// In this case, just show an alert dialog box
+	// displaying the value of the message field in the response
+	function sayHelloCallback(response) {
+		alert(response.greeting);
+	}
+}());
